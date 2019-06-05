74e7059e2e9aed22a4fff6a9fbc9464df525e211
Martin Gaston <squidman@gmail.com>
2019-06-05 10:22:51 +0100

    add moduledoc string

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,4 +1,10 @@
 defmodule TicTacToe.CLI do
+  @moduledoc """
+  A two-player version of Tic Tac Toe running over the command line:
+
+  ./tic_tac_toe
+  """
+
   def main(_) do
     IO.puts("Hello World!")
   end
