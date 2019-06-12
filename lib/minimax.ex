3c482dd3080b588668899714261ba2205ca81481
Martin Gaston <squidman@gmail.com>
2019-06-12 12:27:47 +0100

    refactor to use Minimax struct for args

--- lib/minimax.ex
+++ lib/minimax.ex
@@ -3,30 +3,45 @@ defmodule Minimax do
   @losing_score -10
   @draw_score 0
 
-  def minimax(args, :active, :maximising_player) do
+  @enforce_keys [:board, :maximising_player, :minimising_player]
+  defstruct [:board, :maximising_player, :minimising_player]
+
+  def new(board, maximising_player_mark, minimising_player_mark) do
+    %Minimax{
+      board: board,
+      maximising_player: maximising_player_mark,
+      minimising_player: minimising_player_mark
+    }
+  end
+
+  def best_position(args) do
+    minimax(args, :active, :maximising_player).position
+  end
+
+  defp minimax(args, :active, :maximising_player) do
     traverse(args, :minimising_player, &Enum.max_by/3)
   end
 
-  def minimax(args, :active, :minimising_player) do
+  defp minimax(args, :active, :minimising_player) do
     traverse(args, :maximising_player, &Enum.min_by/3)
   end
 
-  def minimax(args, :won, :maximising_player) do
+  defp minimax(args, :won, :maximising_player) do
     %{score: @losing_score - Board.moves?(args.board)}
   end
 
-  def minimax(args, :won, :minimising_player) do
+  defp minimax(args, :won, :minimising_player) do
     %{score: @winning_score + Board.moves?(args.board)}
   end
 
-  def minimax(_, :drawn, _), do: %{score: @draw_score}
+  defp minimax(_, :drawn, _), do: %{score: @draw_score}
 
-  def traverse(args, next_player, reducer) do
+  defp traverse(args, next_player, reducer) do
     # TODO there's got to be a better way of handling this - I like matching on the atom, but maybe we can bind the atom and the mark into a tuple
     mark =
       case next_player do
-        :maximising_player -> Map.get(args.players, :minimising_player)
-        :minimising_player -> Map.get(args.players, :maximising_player)
+        :maximising_player -> args.minimising_player
+        :minimising_player -> args.maximising_player
       end
 
     Board.available_positions(args.board)
--- lib/player_minimax.ex
+++ lib/player_minimax.ex
@@ -1,18 +1,13 @@
 defmodule PlayerMinimax do
   def move(board, _message, args, _io \\ :stdio) do
     # TODO I do not think mangaging dependencies like this is quite right - I'm leaning towards making a struct with required fields.
-    args = %{
-      board: board,
-      players: %{
-        # TODO the args.player and args.opponent is currently a bit of a hack so we can rush to green
-        maximising_player: args.player,
-        minimising_player: args.opponent
-      }
-    }
-
     case Board.status(board) do
-      :active -> Minimax.minimax(args, :active, :maximising_player).position
-      _ -> :error
+      :active ->
+        Minimax.new(board, args.player, args.opponent)
+        |> Minimax.best_position()
+
+      _ ->
+        :error
     end
   end
 end
