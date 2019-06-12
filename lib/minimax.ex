60e2f847d6509817174ac183bd78e2acb1a2707e
Martin Gaston <squidman@gmail.com>
2019-06-12 14:08:00 +0100

    extract mark into separate function

--- lib/minimax.ex
+++ lib/minimax.ex
@@ -3,14 +3,15 @@ defmodule Minimax do
   @losing_score -10
   @draw_score 0
 
-  @enforce_keys [:board, :maximising_player, :minimising_player]
-  defstruct [:board, :maximising_player, :minimising_player]
+  defstruct [:board, :players]
 
   def new(board, maximising_player_mark, minimising_player_mark) do
     %Minimax{
       board: board,
-      maximising_player: maximising_player_mark,
-      minimising_player: minimising_player_mark
+      players: %{
+        maximising_player: maximising_player_mark,
+        minimising_player: minimising_player_mark
+      }
     }
   end
 
@@ -37,20 +38,16 @@ defmodule Minimax do
   defp minimax(_, :drawn, _), do: %{score: @draw_score}
 
   defp traverse(args, next_player, reducer) do
-    # TODO there's got to be a better way of handling this - I like matching on the atom, but maybe we can bind the atom and the mark into a tuple
-    mark =
-      case next_player do
-        :maximising_player -> args.minimising_player
-        :minimising_player -> args.maximising_player
-      end
-
     Board.available_positions(args.board)
     |> Enum.map(fn square ->
       # TODO can these be piped at all? this feels rather imperative
-      updated = Board.update(args.board, square, mark)
+      updated = Board.update(args.board, square, mark(args, next_player))
       status = Board.status(updated)
       %{position: square, score: minimax(%{args | board: updated}, status, next_player).score}
     end)
     |> reducer.(fn %{score: score} -> score end, fn -> raise(Enum.EmptyError) end)
   end
+
+  defp mark(%{players: %{minimising_player: mark}}, :maximising_player), do: mark
+  defp mark(%{players: %{maximising_player: mark}}, :minimising_player), do: mark
 end
