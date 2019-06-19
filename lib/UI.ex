a5367aed946809ddab53e3c5caa08d72bf905acd
Martin Gaston <squidman@gmail.com>
2019-06-19 13:58:10 +0100

    dynamically generate board dividers based on board size

--- lib/UI.ex
+++ lib/UI.ex
@@ -1,23 +1,35 @@
 defmodule UI do
-  def draw_board_dynamic(board) do
-    zero_indexed_size = Board.size(board) - 1
-
-    Enum.chunk_every(0..zero_indexed_size, Board.side_length(board))
-    |> Enum.map(fn x -> row(board, x) end)
-    |> Enum.join("\n" <> header() <> "\n")
-  end
-
   def draw_board(board) do
-    [
-      header(),
-      draw_board_dynamic(board),
-      header(),
-      ""
-    ]
-    |> Enum.join("\n")
+    zero_indexed_size = Board.size(board) - 1
+    side_length = Board.side_length(board)
+
+    calculate_board_rows(zero_indexed_size, side_length)
+    |> generate_rows(board)
+    |> generate_dividers(side_length)
+    |> add_header_and_footer(side_length)
   end
 
-  defp header(), do: "+-----------+"
+  defp calculate_board_rows(size, side_length) do
+    Enum.chunk_every(0..size, side_length)
+  end
+
+  defp generate_rows(board_list, board), do: Enum.map(board_list, &row(board, &1))
+
+  defp generate_dividers(board_list, side_length) do
+    Enum.join(board_list, "\n" <> divider(side_length) <> "\n")
+  end
+
+  defp add_header_and_footer(board_string, side_length) do
+    "#{divider(side_length)}\n#{board_string}\n#{divider(side_length)}\n"
+  end
+
+  defp divider(side_length) do
+    square_width = 4
+    final_border_width = 1
+    edges_total = 2
+    width = square_width * side_length + final_border_width - edges_total
+    "+" <> Enum.reduce(1..width, "", fn _, acc -> acc <> "-" end) <> "+"
+  end
 
   defp row(board, range) do
     Enum.reduce(range, "", fn pos, acc -> acc <> square(board, pos) end) <> "|"
